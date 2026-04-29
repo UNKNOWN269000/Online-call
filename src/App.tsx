2f0513358c0ab62cadb649a5621aaa8766c33250
UNKNOWN269000 <102501982+UNKNOWN269000@users.noreply.github.com>
2026-04-29 22:33:54 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CallInterface from './components/CallInterface';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <CallInterface />
+    </div>
+  );
+}
+
+export default App;
